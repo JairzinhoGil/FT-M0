408ebb7e1a5086427e99af889554aef8c7544fd5
Jair Higuera <jair.gil.higuera@gmail.com>
2021-07-06 13:41:27 -0500

    tarea4 js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -153,15 +153,17 @@ function fizzBuzz(numero) {
   // De lo contrario, devuelve el numero
   let i = numero;
 
-  if (i % 3 === 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    return "fizzbuzz";
+  }
+
+  else if (i % 3 === 0) {
     return "fizz";
   }
   else if (i % 5 === 0) {
     return "buzz";
   }
-  else if (i % 5 === 0 ) && (i % 3 === 0) {
-    return "fizzbuzz";
-  }
+ 
   else {
     return numero;
   }
@@ -174,6 +176,19 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+if (num1 === 0 || num2 === 0 || num3 === 0) {
+return "Error";
+} else if (num1 < 0 || num2 < 0 || num3 < 0) {
+  return "Hay negativos";
+}
+else if (num1 > num2 &&  num1 > num3) {
+  return "Número 1 es mayor y positivo";
+}
+else if (num3 > num1 && num3 > num2) {
+  return num3 + 1;
+}else{
+  return false;
+}
 }
 
 function esPrimo(numero) {
@@ -182,18 +197,32 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-}
+if (numero < 2 )  return false;
+
+for (var i = 3; i < numero; i++) {
+  if (numero % i === 0) {
+    return false;
+  }
+}return true;
+
+};
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+if (valor) {
+  return "Soy verdadero";
+} 
+else {
+  return "Soy falso";
+}
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  return numero < 999 &&  numero > 99
   
 }
 
@@ -201,7 +230,15 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  //return numero + 5 * 8
+let contador = 1
+  while (contador <= 8) {
+    numero = numero + 5;
+    contador = contador + 1;
+  }
+  return numero
 }
+
 
 
 // No modificar nada debajo de esta línea
